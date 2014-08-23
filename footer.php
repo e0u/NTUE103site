de5e93c9e18206968891407779785547ea7e6546
idreamers <idreamers@dreamtech.tw>
2014-08-23 16:06:40 +0800

    斷行

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 <h6>NTUE新生資訊站 Ver 1.0<br />
 國北教 數資四 
 <a xmlns:cc="http://creativecommons.org/ns#" href="https://www.facebook.com/dreammner.li" property="cc:attributionName" rel="cc:attributionURL">iDreamers </a>製作。
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0 </a> 。
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0 </a> 。<br />
 更新資料請來信通知或使用 FB 通知亦可，謝謝
 </h6>
 </center>
