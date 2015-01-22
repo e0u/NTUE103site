bcb7e7aa257c5ed479ed72b2a17a0eb95a9fa6c2
idreamers <idreamers@dreamtech.tw>
2015-01-22 20:24:01 +0800

    update

---
+++ coruse/index.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Hello World!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">DreamTECH</h3>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="#">Home</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h2 class="cover-heading">課表匯入完成...</h2>
+            <textarea class="form-control" rows="22">
+數學軟體應用與製作 必修  2
+數位教材設計與製作 必修  3
+資料結構  必修  3
+作業系統  必修  3
+基礎程式設計  必修  2
+服務學習(三) 必修  0
+微積分 必修  3
+線性代數  必修  3
+網頁設計與製作 必修  3
+職場倫理與職場精神 選修  3
+數學課程設計  選修  3
+德文(一) 選修  2
+商用英文  選修  2
+行政法(二)  選修  2
+民法債篇  選修  3
+------------------------------------------    
+國民小學語文教材教法--國語教材教法  必修  2
+國民小學數學教材教法  必修  2
+教育社會學 選修  2
+自然科學概論  選修  2
+藝術概論  選修  2
+</textarea>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+                      <?php include_once("footer.php") ?>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
