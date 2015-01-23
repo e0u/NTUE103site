a4f041795befc8ec8c1444e3579f240ea8fc127f
idreamers <idreamers@dreamtech.tw>
2015-01-23 10:33:22 +0800

    99 add

---
+++ maps/admin99/aboutedit.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>後台管理</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/offcanvas.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript">var _jf = _jf || [];_jf.push(['p','30785']);_jf.push(['_setFont','xingothic-tc-w6','css','.xingothic-tc-w6']);_jf.push(['_setFont','xingothic-tc-w6','alias','jfonts']);(function(f,q,c,h,e,i,r,d){var k=f._jf;if(k.constructor===Object){return}var l,t=q.getElementsByTagName("html")[0],a=function(u){for(var v in k){if(k[v][0]==u){if(false===k[v][1].call(k)){break}}}},j=/\S+/g,o=/[\t\r\n\f]/g,b=/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,g="".trim,s=g&&!g.call("\uFEFF\xA0")?function(u){return u==null?"":g.call(u)}:function(u){return u==null?"":(u+"").replace(b,"")},m=function(y){var w,z,v,u,x=typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):" ";if(z){u=0;while((v=w[u++])){if(z.indexOf(" "+v+" ")<0){z+=v+" "}}t[c]=s(z)}}},p=function(y){var w,z,v,u,x=arguments.length===0||typeof y==="string"&&y;if(x){w=(y||"").match(j)||[];z=t[c]?(" "+t[c]+" ").replace(o," "):"";if(z){u=0;while((v=w[u++])){while(z.indexOf(" "+v+" ")>=0){z=z.replace(" "+v+" "," ")}}t[c]=y?s(z):""}}},n;k.push(["_eventActived",function(){p(h);m(e)}]);k.push(["_eventInactived",function(){p(h);m(i)}]);k.addScript=n=function(u,A,w,C,E,B){E=E||function(){};B=B||function(){};var x=q.createElement("script"),z=q.getElementsByTagName("script")[0],v,y=false,D=function(){x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;a("_eventInactived");B()};if(C){v=setTimeout(function(){D()},C)}x.type=A||"text/javascript";x.async=w;x.onload=x.onreadystatechange=function(G,F){if(!y&&(!x.readyState||/loaded|complete/.test(x.readyState))){y=true;if(C){clearTimeout(v)}x.src="";x.onerror=x.onload=x.onreadystatechange=null;x.parentNode.removeChild(x);x=null;if(!F){setTimeout(function(){E()},200)}}};x.onerror=function(H,G,F){if(C){clearTimeout(v)}D();return true};x.src=u;z.parentNode.insertBefore(x,z)};a("_eventPreload");m(h);n(r,"text/javascript",false,3000)})(this,this.document,"className","jf-loading","jf-active","jf-inactive","//ds.justfont.com/js/stable/v/4.7/id/119794050681");</script>
+  </head>
+
+  <body>
+    <?php include_once("analyticstracking.php") ?>
+    <?php include_once("navbar.php") ?>
+
+    <div class="container"> 
+    <form class="form-editabout">
+		<div class="form-group has-success has-feedback">
+        <label class="control-label col-sm-1" for="inputSuccess3">標題</label>
+        <div class="col-sm-3">
+          <input type="text" class="form-control" placeholder="請輸入標題" value="大學部簡史與發展特色">
+        </div>
+        <textarea rows="20" class="form-control" >　　本校負國民小學師資培育之責，由於歷史悠久，學風優良，培育無數優良國民小學師資及教育行政人員，在我國師資培育上扮演積極的角色。76.7.1改制為師範學院，成立數理教育學系，招收高中畢業生，畢業後授與學士學位，以提昇國民小學數理師資素質。
+
+　　80.7.1起，本校改隸為國立後，本學系無論在師資的延聘、設備的添置、學生生活的充實，以及研究進修的強化等各方面，均有顯著的進步。
+
+　　85學年度起，數理教育學系於大學聯考時即分Ａ、Ｂ組招生，Ａ組學生在專門課程主修數學，Ｂ組學生在專門課程主修自然科學。
+
+　　87學年度起，本校為配合校務發展計畫，俾能營造本校改制成教育大學或綜合大學之有利基礎，將原數理教育學系數學組調整獨立設系，培育具有良好數學教育和資訊之素養，以及具奉獻心力、智慧、意願與能力之優秀數學及資訊教育種子教師。
+
+　　93學年度起，數學教育學系調整為數學暨資訊教育學系，於大學考試分發入學招生時，分成數學組、資訊組兩組招生，數學組學生主修數學及數學教育，資訊組學生主修資訊及資訊教育。
+
+　　本系為我國小學數學暨資訊師資培育的搖籃。本系分數學、資訊兩組，課程設計考量學生興趣、性向及生涯進路發展。學生進路除學校外，亦可選擇至企業界擔任數理及資訊教育相關工作。
+
+　　本系教師具數學及數學教育、資訊及資訊教育等專長；畢業學生除具備數學思考、分析推理及問題解決等基本能力外，亦具數學、資訊與教育領域之教學與課程規劃等專業能力。本系擁有極多的數學教育相關圖書儀器、教材及研究成果，教學資源相當豐富。優良傳承之數學梯隊營亦為本系特色之一，寒、暑假可提供系上學生發揮創意、體驗教學實務之機會。
+        </textarea>
+        <div class="form-group has-success has-feedback">
+        <label class="control-label col-sm-1" for="inputSuccess3">標題</label>
+        <div class="col-sm-3">
+          <input type="text" class="form-control" placeholder="請輸入標題" value="碩士班簡史與發展特色">
+        </div>
+        <div class="alert alert-danger fade in" role="alert">
+          <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <strong>error：</strong> file:aboutmaster.php - 404 .。
+          </div>
+        <textarea rows="20" class="form-control" >87學年度本校奉准設立數理教育學系碩士班，並於民國87年5月招生。同年，本校旋又奉准將原數理教育學系數學組（A組）及自然組（B組）分立設置數學教育學系及自然科學教育學系，並將數理教育學系碩士班更名為數理教育研究所；數理教育研究所於93學年度經教育部核定通過採總量管制發展方式，新增系所為數學教育研究所（調整更名）及自然科學教育研究所（調整更名）。本碩士班於99學年度系所整併為數學暨資訊教育學系數學教育碩士班。103學年度更名調整為數學暨資訊教育學系碩士班。
+本碩士班以數學與資訊教育課程、教材設計、教學策略、教學科技、課程評鑑、學習評量及教師專業發展為研究重點，致力於數學與資訊教育教學理論之基礎研究及實務改進之實徵研究。
+
+        </textarea>
+	<button type="submit" onClick='alert("存檔成功")'  class="btn btn-primary">存檔</button>
+    </form>
+    
+      <div class="row row-offcanvas row-offcanvas-right">
+        <div class="col-xs-10 col-sm-9">
+   
+    
+
+    </div></div>
+
+
+    </div><!--/.container-->
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/offcanvas.js"></script>
+
+  </body>
+</html>
