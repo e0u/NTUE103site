d3a47d82cb68ef102913a191fe7596dd85ae91af
idreamers <idreamers@dreamtech.tw>
2015-06-16 10:40:18 +0800

    added 108級新生資訊站

---
+++ NTUE104/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>國立臺北教育大學 2015 新生資訊站</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/offcanvas.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <?php include_once("analyticstracking.php") ?>
+    <?php include_once("navbar.php") ?>
+
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <!--
+          <div class="alert alert-info fade in" role="alert">
+          <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <strong>目前進行事項：</strong> 全校第三階段選課，請至<a href="http://apstu.ntue.edu.tw/univweb/Secure/default.aspx">校務資訊系統</a>內進行選課。</br>
+          路徑：登入校務系統後 > 線上選課 > 第三階段選課 進行選課。 本階段為即時選課。(全校搶課)
+          </div>
+          
+          <div class="alert alert-success fade in" role="alert">
+          <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <strong>目前有的社團活動：</strong> 管樂社 <a href="https://www.facebook.com/groups/ntue107/permalink/1517522428459400/"><font size:13px>-08.23 嘉義管樂節 熱血報名中 </font></a></br>
+          
+          </div>
+        -->
+          <div class="jumbotron">
+            <h2>NTUE 新生資訊站<p style="font-size:17px;" class="label label-primary">**非官方**</p></h2>
+            <font color="red">本網站目前建置中... 資料將陸續更新</font>
+            <p>Hello, 新生! 覺得學校的資訊總是亂糟糟？這裡提供了一個統整的平台加以彙整各系的資訊，讓大家可以更輕易知道各種消息。由於本站屬於非官方性質，故不負任何的責任，如有任何錯誤也歡迎通知作者進行更新。如有各系系學會、系助教、各單位有資訊要需要協助公布，請來信 im@dreamtech.tw ，謝謝。</p>
+          </div>
+          <div class="row">
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>網站公告~</h2>
+              <p><span class="label label-success">系統</span> <font size:13px>-06.16 網站對外上線 </font> </p>             
+              <p><a class="btn btn-default" href="#" role="button">查看更多 &raquo;</a></p>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>學校資訊</h2>
+              <!--
+              <p><span class="label label-danger">HOT!!</span> <a href="107stuid.pdf"> <font size:13px> 大學部新生學號 </font></a> </p>
+              <p><span class="label label-success">NEW!!</span> <a href="http://academic.ntue.edu.tw/files/14-1007-3359,r12-1.php"> 103學年度大一「英文」分班</font></a> </p>
+              <p><span class="label label-default">宿舍</span> <font size:13px> 8/19-8/25 宿舍上網登錄申請 </font> </p>
+            </div>
+          -->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>各系公告</h2>
+              <!--
+              <p><span class="label label-primary">系統</span> <font size:13px>-08.12 我是測試消息，不要理我 </font> </p>
+              
+              <p><span class="label label-warning">教育系</span> <font size:13px>-08.12 早安早安你好嗎 </font> </p>
+              <p><span class="label label-default">語創系</span> <font size:13px>-08.12 標題不含日期有十一個字 </font> </p>
+              <p><span class="label label-success">藝設系</span> <font size:13px>-08.12 其實我是放來充版面的 </font> </p>
+              
+            </div>
+          -->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>各系活動</h2>
+              <!--
+              <p><span class="label label-primary">數資系</span> <font size:13px>-08.20 我是測試消息，不要理我 </font> </p>
+              <p><span class="label label-warning">教育系</span> <font size:13px>-08.19 早安早安你好嗎 </font> </p>
+              <p><span class="label label-default">語創系</span> <font size:13px>-08.19 標題不含日期有十一個字 </font> </p>
+              <p><a class="btn btn-default" href="#" role="button">查看更多 &raquo;</a></p>
+            </div>
+          -->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>社團活動</h2>
+<!--
+              <p><span class="label label-danger">管樂社</span> <a href="https://www.facebook.com/groups/ntue107/permalink/1517522428459400/"><font size:13px>-08.23 嘉義管樂節 熱血報名中 </font></a> </p>
+              <p><span class="label label-primary">數資系</span> <font size:13px>-08.20 一二三四五六七八九零零 </font> </p>
+              <p><span class="label label-default">語創系</span> <font size:13px>-08.19 標題不含日期有十一個字 </font> </p>
+              <p><a class="btn btn-default" href="#" role="button">查看更多 &raquo;</a></p>
+            </div>
+          -->
+            <div class="col-6 col-sm-6 col-lg-4">
+              <h2>About</h2>
+              <p>其實我也不知道為甚麼想做這個，大概是太無聊了吧？雖然我很命苦的在暑修ing...
+                但是看不下去時，會想做點不同的東西來呈現出來...</p>
+              
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">常用連結</a>
+            <a href="http://www.ntue.edu.tw/" class="list-group-item">國北教大首頁</a>
+            <a href="http://academic.ntue.edu.tw/" class="list-group-item">教務處</a>
+            <a href="http://dsa.ntue.edu.tw/web/index.php" class="list-group-item">學務處</a>
+            <a href="http://apstu.ntue.edu.tw/univweb/Secure/default.aspx" class="list-group-item">教務資訊系統</a>
+            <a href="https://www.facebook.com/groups/414033391994003/" class="list-group-item">學治會 FB 社團</a>
+            <a href="https://www.facebook.com/groups/194514594077227/" class="list-group-item">國北選課沒地雷</a>
+            <a href="https://www.facebook.com/NTUECrushes" class="list-group-item">告白國北 NTUE Crushes</a>
+            <a href="https://www.facebook.com/NTUEHate" class="list-group-item">黑特國北 NTUE Hates</a>
+          </div>
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <?php include_once("footer.php") ?>
+      </footer>
+
+    </div><!--/.container-->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/offcanvas.js"></script>
+  </body>
+</html>
