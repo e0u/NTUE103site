05fa039a5887f82c516e76b7ac8774780dac0887
idreamers <idreamers@dreamtech.tw>
2016-12-20 21:44:18 +0800

    105 問卷
    Updated for 1051. First time updated.

---
+++ NTUE105/footer.php
@@ -0,0 +1,12 @@
+<center>
+    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+    <img alt="創用 CC 授權條款" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" >
+</a>
+<br />
+<h6>NTUE新生資訊站 Ver 1.0<br />
+國北教 數資四 
+<a xmlns:cc="http://creativecommons.org/ns#" href="https://www.facebook.com/dreammner.li" property="cc:attributionName" rel="cc:attributionURL">iDreamers </a>製作。
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0 </a> 。<br />
+更新資料請來信通知或使用 FB 通知亦可，謝謝
+</h6>
+</center>
